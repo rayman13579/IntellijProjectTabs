c30ccdf0f37abbd9000769802c65470188d9287b
rayman13579 <roman.reno@hotmail.com>
2024-04-08 11:55:04 +0200

    don't add project asynchronously

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "at.rayman"
-version = "1.0"
+version = "1.1"
 
 repositories {
     mavenCentral()
--- src/main/java/at/rayman/projecttabs/ProjectTabManager.kt
+++ src/main/java/at/rayman/projecttabs/ProjectTabManager.kt
@@ -1,27 +1,20 @@
 package at.rayman.projecttabs
 
 import com.intellij.openapi.actionSystem.ActionManager
-import com.intellij.openapi.actionSystem.ex.ActionManagerEx
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectCloseListener
 import com.intellij.openapi.startup.ProjectActivity
-import kotlinx.coroutines.coroutineScope
 
 class ProjectTabManager : ProjectCloseListener {
 
     companion object {
-        @JvmStatic
         val projectTabGroup: ProjectTabActionGroup
             get() = ActionManager.getInstance().getAction("ProjectTabs") as ProjectTabActionGroup
     }
 
     private class StartupActivity : ProjectActivity {
         override suspend fun execute(project: Project) {
-            coroutineScope {
-                ActionManagerEx.withLazyActionManager(scope = this) {
-                    (it.getAction("ProjectTabs") as ProjectTabActionGroup).addProject(project)
-                }
-            }
+            projectTabGroup.addProject(project)
         }
     }
 
